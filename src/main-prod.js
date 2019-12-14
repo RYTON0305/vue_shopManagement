db3039031e83baa398f0280fcc92cf0af1365f9f
RYTON <ryton.jiang@gmail.com>
2019-12-14 16:51:52 +0800

    为开发模式与发布模式指定不同的打包入口

---
+++ src/main-prod.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import '@/assets/css/global.css'
+import '@/assets/fonts/iconfont.css'
+import axios from 'axios'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+import TreeTable from 'vue-table-with-tree-grid'
+
+import VueQuillEditor from 'vue-quill-editor'
+
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+Vue.component('tree-table', TreeTable)
+Vue.filter('dateFormat', (originVal) => {
+  const dt = new Date(originVal)
+  const year = dt.getFullYear()
+  const month = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const date = (dt.getDate() + '').padStart(2, '0')
+  const hour = (dt.getHours() + '').padStart(2, '0')
+  const minute = (dt.getMinutes() + '').padStart(2, '0')
+  const second = (dt.getSeconds() + '').padStart(2, '0')
+  return `${year}-${month}-${date} ${hour}:${minute}:${second}`
+})
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+axios.interceptors.response.use(config => {
+  // console.log(config)
+  NProgress.done()
+  return config
+})
+Vue.prototype.$http = axios
+
+Vue.use(VueQuillEditor /* { default global options } */)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
